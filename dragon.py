af411da5d1d220c3819b6a4f96cf12325538bb2f
Angus L'Herrou <piraka@brandeis.edu>
2022-10-26 14:22:54 -0500

    replace draw.py with cli-draw

--- dragon.py
+++ dragon.py
@@ -1,11 +1,10 @@
-from draw import Dir, Turn, draw
+from cli_draw import Direction, Turn, draw
 
 
 class Dragon:
-
     def __init__(self, depth: int):
         self.turns = []
-        self.dir: Dir = Dir.R
+        self.dir: Direction = Direction.R
         self.trace(depth)
 
     def trace(self, depth: int):
@@ -20,29 +19,25 @@ class Dragon:
 
     def write(self, turn: Turn):
         self.turns.append(turn)
-        match turn:
-            case Turn.R:
-                self.dir = self.dir.succ()
-            case Turn.L:
-                self.dir = self.dir.pred()
+        self.dir = self.dir.turn(turn)
 
 
 def main(depth=5, x_step=2, y_step=1):
     dragon = Dragon(depth)
-    draw(Dir.R, dragon.turns, x_step, y_step)
+    draw(Direction.R, dragon.turns, x_step, y_step)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description='Draw a dragon curve.')
-    parser.add_argument('depth', type=int,
-                        help='Depth of the dragon curve to generate')
-    parser.add_argument('--x-step', '-x', type=int, default=2,
-                        help='x-axis scaling')
-    parser.add_argument('--y-step', '-y', type=int, default=1,
-                        help='y-axis scaling')
+    parser = argparse.ArgumentParser(
+        description="Draw a dragon curve.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("DEPTH", type=int, help="Depth of the dragon curve to generate")
+    parser.add_argument("--x-step", "-x", type=int, default=2, help="x-axis scaling")
+    parser.add_argument("--y-step", "-y", type=int, default=1, help="y-axis scaling")
 
     args = parser.parse_args()
 
-    main(args.depth, args.x_step, args.y_step)
+    main(args.DEPTH, args.x_step, args.y_step)
