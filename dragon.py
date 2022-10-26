abaf474ba985d5a7cfc72c4e753f1d2b53d88b6e
Angus L'Herrou <piraka@brandeis.edu>
2022-10-26 18:11:34 -0500

    v1.2.0: clean up code; add CLI arguments

--- dragon.py
+++ dragon.py
@@ -1,3 +1,5 @@
+import argparse
+
 from cli_draw import Direction, Turn, draw
 
 
@@ -9,7 +11,6 @@ class Dragon:
 
     def trace(self, depth: int):
         self.trace_depth(depth, Turn.R)
-        print("")
 
     def trace_depth(self, n: int, turn: Turn):
         if n > 0:
@@ -22,22 +23,49 @@ class Dragon:
         self.dir = self.dir.turn(turn)
 
 
-def main(depth=5, x_step=2, y_step=1):
+def main(depth: int = 5,
+         initial_direction: Direction = Direction.R,
+         x_step: int = 2,
+         y_step: int = 1,
+         turns_only: bool = False,
+         ):
     dragon = Dragon(depth)
-    draw(Direction.R, dragon.turns, x_step, y_step)
+    if turns_only:
+        print(''.join(turn.name for turn in dragon.turns))
+    else:
+        draw(initial_direction, dragon.turns, x_step, y_step)
 
 
 if __name__ == "__main__":
-    import argparse
-
     parser = argparse.ArgumentParser(
         description="Draw a dragon curve.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument("DEPTH", type=int, help="Depth of the dragon curve to generate")
-    parser.add_argument("--x-step", "-x", type=int, default=2, help="x-axis scaling")
-    parser.add_argument("--y-step", "-y", type=int, default=1, help="y-axis scaling")
+    parser.add_argument("--turns-only", action="store_true", help="Instead of drawing the curve, output the turns necessary for cli-draw to draw the curve")
+    parser.add_argument(
+        "--x-step",
+        "-x",
+        type=int,
+        default=2,
+        help="The number of columns a single x-axis step takes.",
+    )
+    parser.add_argument(
+        "--y-step",
+        "-y",
+        type=int,
+        default=1,
+        help="The number of lines a single y-axis step takes.",
+    )
+    parser.add_argument(
+        "--initial-direction",
+        "-d",
+        type=str,
+        default="R",
+        choices=Direction._member_names_,
+        help="The starting direction of travel.",
+    )
 
     args = parser.parse_args()
 
-    main(args.DEPTH, args.x_step, args.y_step)
+    main(args.DEPTH, Direction[args.initial_direction], args.x_step, args.y_step, args.turns_only)
